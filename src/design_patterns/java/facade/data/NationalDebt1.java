2f0902ba8f96b0d6c2b1eb167eba7884180edd83
Bihan Li <bihl@microsoft.com>
2022-10-02 23:55:29 +0800

    [Java] Added 12 Facade

---
+++ src/design_patterns/java/facade/data/NationalDebt1.java
@@ -0,0 +1,11 @@
+package design_patterns.java.facade.data;
+
+public class NationalDebt1 {
+    public void sell() {
+        System.out.println("国债1卖出");
+      }
+
+    public void buy() {
+        System.out.println("国债1买入");
+    }
+}
---
+++ src/design_patterns/java/facade/data/Stock1.java
@@ -0,0 +1,11 @@
+package design_patterns.java.facade.data;
+
+public class Stock1 {
+    public void sell() {
+        System.out.println("股票1卖出");
+    }
+
+    public void buy() {
+        System.out.println("股票1买入");
+    }
+}
