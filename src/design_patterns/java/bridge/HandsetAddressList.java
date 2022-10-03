63b2539a489d06edf8fa0b29268e51ea578a6cfe
libihan <786150017@qq.com>
2022-10-03 22:04:03 +0800

    [Java] Added 22 bridge

---
+++ src/design_patterns/java/bridge/HandsetAddressList.java
@@ -0,0 +1,8 @@
+package design_patterns.java.bridge;
+
+public class HandsetAddressList extends HandsetSoft {
+    @Override
+    public void run() {
+        System.out.println("运行手机通讯录");
+    }
+}
---
+++ src/design_patterns/java/bridge/HandsetGame.java
@@ -0,0 +1,8 @@
+package design_patterns.java.bridge;
+
+public class HandsetGame extends HandsetSoft {
+    @Override
+    public void run() {
+        System.out.println("运行手机游戏");
+    }
+}
