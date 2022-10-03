cf5669f7504bb9b90f0966ed3127e6f9ffad6227
libihan <786150017@qq.com>
2022-10-03 22:04:44 +0800

    [Java] Added 28 visitor

---
+++ src/design_patterns/java/visitor/Action.java
@@ -0,0 +1,6 @@
+package design_patterns.java.visitor;
+
+public abstract class Action {
+    public abstract void getManConclusion(Man man);
+    public abstract void getWomanConclusion(Woman woman);
+}
