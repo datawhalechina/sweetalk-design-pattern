a617ba373d59129e0f2a56ae1f0e9a448fd89697
libihan <786150017@qq.com>
2022-10-03 22:03:53 +0800

    [Java] Added 19 composite

---
+++ src/design_patterns/java/composite/Main.java
@@ -0,0 +1,30 @@
+package design_patterns.java.composite;
+
+public class Main {
+    public static void main(String[] args) {
+        ConcreteCompany root = new ConcreteCompany("北京总公司");
+        root.add(new HRDepartment("总公司人力资源部"));
+        root.add(new FinanceDepartment("总公司财务部"));
+
+        ConcreteCompany comp = new ConcreteCompany("上海华东分公司");
+        comp.add(new HRDepartment("华东分公司人力资源部"));
+        comp.add(new FinanceDepartment("华东分公司财务部"));
+        root.add(comp);
+
+        ConcreteCompany comp1 = new ConcreteCompany("南京办事处");
+        comp1.add(new HRDepartment("南京办事处人力资源部"));
+        comp1.add(new FinanceDepartment("南京办事处财务部"));
+        root.add(comp1);
+
+        ConcreteCompany comp2 = new ConcreteCompany("杭州办事处");
+        comp2.add(new HRDepartment("杭州办事处人力资源部"));
+        comp2.add(new FinanceDepartment("杭州办事处财务部"));
+        root.add(comp2);
+
+        System.out.println("\n结构图：");
+        root.display(1);
+
+        System.out.println("\n职责：");
+        root.lineOfDuty();
+  }
+}
