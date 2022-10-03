76b37d28756407d5d8a4a931c736d885d42d64ec
libihan <786150017@qq.com>
2022-10-03 22:03:50 +0800

    [Java] Added 18 memento

---
+++ src/design_patterns/java/memento/GameRole.java
@@ -0,0 +1,68 @@
+package design_patterns.java.memento;
+
+public class GameRole {
+    // 生命力
+    private int vit;
+    // 攻击力
+    private int atk;
+    // 防御力
+    private int def;
+
+    public int getVit() {
+        return vit;
+    }
+    public void setVit(int vit) {
+        this.vit = vit;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    // 状态显示
+    public void stateDisplay() {
+        System.out.println("当前角色状态：");
+        System.out.println("体力：" + this.vit);
+        System.out.println("攻击力：" + this.atk);
+        System.out.println("防御力：" + this.def);
+        System.out.println();
+    }
+
+    // 获得初始状态
+    public void getInitState() {
+        this.vit = 100;
+        this.atk = 100;
+        this.def = 100;
+    }
+
+    // 战斗
+    public void fight() {
+        this.vit = 0;
+        this.atk = 0;
+        this.def = 0;
+    }
+
+    // 保存游戏状态
+    public RoleStateMemento saveState() {
+        return new RoleStateMemento(vit, atk, def);
+    }
+
+    // 恢复角色状态
+    public void recoveryState(RoleStateMemento memento) {
+        this.vit = memento.getVit();
+        this.atk = memento.getAtk();
+        this.def = memento.getDef();
+    }
+}
---
+++ src/design_patterns/java/memento/RoleStateCaretaker.java
@@ -0,0 +1,13 @@
+package design_patterns.java.memento;
+
+public class RoleStateCaretaker {
+    private RoleStateMemento memento;
+
+    public RoleStateMemento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(RoleStateMemento memento) {
+        this.memento = memento;
+    }
+}
