d6aa1b65685938f198af86662190f93e19021ec9
libihan <786150017@qq.com>
2022-10-03 22:03:22 +0800

    [Java] Added 12 Facade

---
+++ src/design_patterns/java/facade/data/Stock3.java
@@ -0,0 +1,11 @@
+package design_patterns.java.facade.data;
+
+public class Stock3 {
+    public void sell() {
+        System.out.println("股票3卖出");
+    }
+
+    public void buy() {
+        System.out.println("股票3买入");
+    }
+}
