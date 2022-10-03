16f636b33f3d3fe8838ad114246ac3a83c990fd1
libihan <786150017@qq.com>
2022-10-03 22:03:50 +0800

    [Java] Added 18 memento

---
+++ src/design_patterns/java/memento/RoleStateMemento.java
@@ -0,0 +1,39 @@
+package design_patterns.java.memento;
+
+public class RoleStateMemento {
+    private int vit;
+    private int atk;
+    private int def;
+
+    public int getVit() {
+        return vit;
+    }
+
+    public void setVit(int vit) {
+        this.vit = vit;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public RoleStateMemento(int vit, int atk, int def) {
+        this.vit = vit;
+        this.atk = atk;
+        this.def = def;
+    }
+
+    public RoleStateMemento() {}
+}
